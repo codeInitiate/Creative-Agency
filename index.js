bcdab1d2b43f821b95bb374414c6fef99b57ec70
Code_Initiative <wolfx15.tm@gmail.com>
2021-11-08 16:03:00 +0000

    index.js

--- index.js
+++ index.js
@@ -12,31 +12,58 @@ gsap.from(".contain_img img", {
   x: -2000,
 });
 
+gsap.from(".imgOne", {
+  duration: 5,
+  ease: "power2-out",
+  x: 2000,
+  delay: 2,
+});
+
+gsap.from(".imgTwo", {
+  duration: 6,
+  ease: "power2-out",
+  x: 2000,
+  delay: 2,
+});
+
+gsap.from(".contain_offset h2 ", {
+  duration: 5,
+  ease: "power2-out",
+  x: -1000,
+  delay: 1,
+});
+
+gsap.from(".contain_offset p ", {
+  duration: 6,
+  ease: "power2-out",
+  x: -2000,
+  delay: 1,
+});
 // This Is For The Gsap Animation Code !!!!
 
 // This Is The Code For The Fade-In On Scroll Observer !!!!!!
 
-// const options = {
-//   threshold: 1,
-// };
-// const faders = document.querySelectorAll(".contain_img");
-// const appearOnScroll = new IntersectionObserver(function (
-//   entries,
-//   apperOnScroll
-// ) {
-//   entries.forEach((entry) => {
-//     if (!entry.isIntersecting) {
-//       return;
-//     } else {
-//       entry.target.classList.add("appear");
-//       appearOnScroll.unobserve(entry.target);
-//     }
-//   });
-// },
-// options);
+const options = {
+  threshold: 0,
+};
+const faders = document.querySelectorAll(".contain_offset");
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  apperOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+options);
 
-// faders.forEach((fader) => {
-//   appearOnScroll.observe(fader);
-// });
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
 
 // This Is The Code For The Fade-In On Scroll Observer !!!!!!
